c35ffaf6bd6185beac9270a9b909e06adf606c44
yi-xu-0100 <xjy123456120@163.com>
2018-01-26 12:48:53 +0800

    修复gist插入问题
    修复table显示问题
    调节highlight和element设置冲突

--- scripts/CustomGist.js
+++
@@ -1,5 +0,0 @@
-hexo.extend.tag.register('gist', function (args) {
-  var id = args.shift();
-  var file = args.length ? '?file=' + args[0] : '';
-  return '<code data-gist-id="' + id + '" class="highlight plain"></code>';
-});
